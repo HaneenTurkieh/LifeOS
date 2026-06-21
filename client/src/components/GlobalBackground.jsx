5a8b29238908d04a14371344319b2cb1326ffa4f
HaneenTurkieh <haneenturkieh@hotmail.com>
2026-06-21 15:16:02 +0300

    Scale up Spline background, update theme-color to violet brand hue

--- client/src/components/GlobalBackground.jsx
+++ client/src/components/GlobalBackground.jsx
@@ -4,7 +4,7 @@ const Spline = lazy(() => import('@splinetool/react-spline'));
 
 export default function GlobalBackground() {
   return (
-    <div className="w-screen h-screen fixed top-0 left-0 -z-10 pointer-events-none">
+    <div className="w-screen h-screen fixed top-0 left-0 -z-10 pointer-events-none transform scale-[1.35] md:scale-150 origin-center">
       <Suspense fallback={<div className="w-full h-full bg-[#0c0a1a]" />}>
         <Spline scene="https://prod.spline.design/H-SRwMBChSgp1uzm/scene.splinecode" />
       </Suspense>
